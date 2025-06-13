314558320c3bbb06599fb4f5aae99cc10defffc1
Octopupu5 <pankratovstepan2004@gmail.com>
2025-06-13 16:03:14 +0300

    added flexibility for generating data

--- dummy_data/generate.py
+++ dummy_data/generate.py
@@ -1,10 +1,27 @@
 import os
 from pathlib import Path
 import subprocess
+import sys
+
+def find_ffmpeg():
+    try:
+        subprocess.run(['ffmpeg', '-version'], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        return 'ffmpeg'
+    except Exception:
+        pass
+
+    ffmpeg_path = Path("ffmpeg.exe")
+    if ffmpeg_path.exists():
+        return str(ffmpeg_path.absolute())
+    parent_ffmpeg = Path("../ffmpeg.exe")
+    if parent_ffmpeg.exists():
+        return str(parent_ffmpeg.absolute())
+    sys.exit(1)
 
 def make_dummy_mp3(path, duration_sec=30 * 2 * 16):
+    ffmpeg = find_ffmpeg()
     cmd = [
-        'ffmpeg',
+        ffmpeg,
         '-hide_banner',
         '-loglevel', 'error',
         '-y',
